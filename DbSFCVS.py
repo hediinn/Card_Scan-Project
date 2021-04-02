481abd1f0c9b975f2c89003d7bfa3ce3363fe4a8
Hediinn <hediinn@gmail.com>
2021-04-02 21:40:24 +0100

    error test

--- DbSFCVS.py
+++ DbSFCVS.py
@@ -30,7 +30,7 @@ class Scryfall():
     self.parM = {'q':str(self.name)}
     self.reqa = req.api.get("https://api.scryfall.com/cards/search",params=self.parM)
     if self.reqa.status_code!= 200:
-      raise ApiError('Cannot fetch all tasks: {}'.format(self.reqa.status_code))
+      raise ConnectionAbortedError('Cannot fetch all tasks: {}'.format(self.reqa.status_code))
     self.realName = self.reqa.json()['data'][0]['name']
     self.count = self.reqa.json()['total_cards']
 
